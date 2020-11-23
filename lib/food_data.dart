5c9da3a8916441f818353aa7dc35cc3c068c0ddf
Jordan Seiler <jordanseiler@jordans-mbp.lan>
2020-11-22 18:25:27 -0600

    Add twitter feed and RSC data

---
+++ lib/food_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class FoodData {
+  static final rscData = [
+    {
+      'location': "Chick-fil-A",
+      'Sun': 'Closed',
+      'Mon': '7:00a - 7:00p',
+      'Tue': '7:00a - 7:00p',
+      'Wed': '7:00a - 7:00p',
+      'Thu': '7:00a - 7:00p',
+      'Fri': '7:00a - 5:00p',
+      'Sat': '11:00a - 2:00p',
+    },
+    {
+      'location': "Freddy's",
+      'Sun': 'Closed',
+      'Mon': '10:00a - 2:00p',
+      'Tue': '10:00a - 2:00p',
+      'Wed': '10:00a - 2:00p',
+      'Thu': '10:00a - 2:00p',
+      'Fri': '10:00a - 2:00p',
+      'Sat': 'Closed',
+    },
+    {
+      'location': "Panda Express",
+      'Sun': 'Closed',
+      'Mon': '10:00a - 7:00p',
+      'Tue': '10:00a - 7:00p',
+      'Wed': '10:00a - 7:00p',
+      'Thu': '10:00a - 7:00p',
+      'Fri': '10:00a - 5:00p',
+      'Sat': 'Closed',
+    },
+    {
+      'location': "Pizza Hut",
+      'Sun': 'Closed',
+      'Mon': '10:00a - 2:00p',
+      'Tue': '10:00a - 2:00p',
+      'Wed': '10:00a - 2:00p',
+      'Thu': '10:00a - 2:00p',
+      'Fri': '10:00a - 2:00p',
+      'Sat': 'Closed',
+    },
+    {
+      'location': "Starbucks",
+      'Sun': 'Closed',
+      'Mon': '7:00a - 7:00p',
+      'Tue': '7:00a - 7:00p',
+      'Wed': '7:00a - 7:00p',
+      'Thu': '7:00a - 7:00p',
+      'Fri': '7:00a - 5:00p',
+      'Sat': '9:00a - 2:00p',
+    },
+  ];
+}
