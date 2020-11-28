a2fdee10d555518101820f519056a488ced44b93
Jordan Seiler <jordanseiler@jordans-mbp.lan>
2020-11-28 12:13:21 -0600

    Adds dynamic greeting to homepage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -16,10 +16,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   //Creating an instance for Firestore, students
   //Tapping into our collection of Students
-  CollectionReference students = FirebaseFirestore.instance.collection('Students');
+  CollectionReference students =
+      FirebaseFirestore.instance.collection('Students');
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +29,14 @@ class _HomePageState extends State<HomePage> {
         elevation: 0.0,
         brightness: Brightness.light,
         iconTheme: IconThemeData(color: shockerBlack),
-        actions: <Widget> [
+        actions: <Widget>[
           IconButton(
-            icon : const Icon(Icons.wb_sunny),
+            icon: const Icon(Icons.wb_sunny),
             color: shockerYellow,
             tooltip: 'Weather',
             onPressed: () {
               Navigator.of(context)
-                  .push(
-                MaterialPageRoute(
-                  builder: (context) => WeatherApp()
-                )
-              );
+                  .push(MaterialPageRoute(builder: (context) => WeatherApp()));
             },
           )
         ],
@@ -56,7 +52,7 @@ class _HomePageState extends State<HomePage> {
               SizedBox(
                 height: 100,
               ),
-              Greeting('User'),
+              Greeting(),
               SizedBox(
                 height: 100,
               ),
@@ -86,14 +82,16 @@ class _HomePageState extends State<HomePage> {
 }
 
 class Greeting extends StatefulWidget {
-  final String name;
-  Greeting(this.name);
-
   @override
   _GreetingState createState() => _GreetingState();
 }
 
 class _GreetingState extends State<Greeting> {
+  //Variables for greeting statement
+  TimeOfDay now = TimeOfDay.now();
+  int noon = 12;
+  int fivePM = 17;
+
   //Setting up Greeting Widget to retrieve data from FireBase
 
   //Creating a Future function that is going to return a variable type of "DocumentSnapshot"
@@ -106,7 +104,8 @@ class _GreetingState extends State<Greeting> {
 
     //Get the current logged in student's data
     //They are targeted by their "_auth.uid"
-    final student = FirebaseFirestore.instance.collection('Students').doc(_auth.uid).get();
+    final student =
+        FirebaseFirestore.instance.collection('Students').doc(_auth.uid).get();
     return await student;
   }
 
@@ -116,24 +115,53 @@ class _GreetingState extends State<Greeting> {
       //Set future variable as our Future<DocumentSnapshot>
       future: getUserFirstName(),
 
-      builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot){
+      builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
         //Creating a Map variable with key being a String and Value being Dynamic
         //The data variable is set to the documents (Data like their first name and classes) inside the collection (Students)
         Map<String, dynamic> data = snapshot.data.data();
 
         //If-Else statement to handle if the connection is valid or not
-        if(snapshot.connectionState == ConnectionState.done){
-          //Return Text field targeting Student's "First Name" document
-          return Text(
-            "Hello,\n ${data['First Name']}",
-            style: GoogleFonts.josefinSans(
-              fontSize: 50,
-              fontWeight: FontWeight.w900,
-              color: shockerBlack,
-            ),
-          );
-        } else if (snapshot.connectionState == ConnectionState.none){
-          return Text('Null');
+        if (snapshot.connectionState == ConnectionState.done) {
+          //If-Else statements to change the greeting per time-of-day
+          if (now.hour < noon) {
+            return Text(
+              "Good morning,\n${data['First Name']}",
+              style: GoogleFonts.josefinSans(
+                fontSize: 50,
+                fontWeight: FontWeight.w900,
+                color: shockerBlack,
+              ),
+            );
+          } else if (now.hour >= noon && now.hour < fivePM) {
+            return Text(
+              "Good afternoon,\n${data['First Name']}",
+              style: GoogleFonts.josefinSans(
+                fontSize: 50,
+                fontWeight: FontWeight.w900,
+                color: shockerBlack,
+              ),
+            );
+          } else if (now.hour >= fivePM) {
+            return Text(
+              "Good evening,\n${data['First Name']}",
+              style: GoogleFonts.josefinSans(
+                fontSize: 50,
+                fontWeight: FontWeight.w900,
+                color: shockerBlack,
+              ),
+            );
+          } else {
+            return Text(
+              "Hello,\n${data['First Name']}",
+              style: GoogleFonts.josefinSans(
+                fontSize: 50,
+                fontWeight: FontWeight.w900,
+                color: shockerBlack,
+              ),
+            );
+          }
+        } else if (snapshot.connectionState == ConnectionState.none) {
+          return Text('Hello');
         }
         return Text('Null');
       },
