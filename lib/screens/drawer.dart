ff245980728d55d38cd6bf133fc650c3093a87ed
Jordan Seiler <jordanseiler@jordans-mbp-2.lan>
2021-03-09 18:44:34 -0600

    front end dev for course pages

--- lib/screens/drawer.dart
+++ lib/screens/drawer.dart
@@ -7,64 +7,106 @@ import 'package:firebase_auth/firebase_auth.dart';
 import './login_page.dart';
 import './calendar_page.dart';
 import './weather.dart';
+import './course_page.dart';
 
 class CustomDrawer extends StatelessWidget {
-
   final _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-            child: ListView(
-              children: [
-                DrawerHeader(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: <Color>[
-                        shockerYellow,
-                        shockerYellow
-                      ])
+        child: ListView(
+      children: [
+        DrawerHeader(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: <Color>[shockerYellow, shockerYellow])),
+            child: Container(
+              child: Column(
+                children: [
+                  Material(
+                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    elevation: 10,
+                    child: Image.asset(
+                      'images/wsulogo.png',
+                      width: 100,
+                      height: 100,
                     ),
-                    child: Container (
-                      child: Column(
-                        children: [
-                           Material(
-                             borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                             elevation: 10,
-                             child: Image.asset('images/wsulogo.png', width: 100, height: 100,),
-                           ),
-                          Padding(padding: EdgeInsets.all(5.0), 
-                            child: Text('WSUGo', style: TextStyle(color: Colors.white, fontSize: 21.0),),)
-                        ],
-                      ),
-                   )),  
-                CustomListTile(Icons.home,'Home', ()=>{
-                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => HomePage()))
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Text(
+                      'WSUGo',
+                      style: TextStyle(color: Colors.white, fontSize: 21.0),
+                    ),
+                  )
+                ],
+              ),
+            )),
+        CustomListTile(
+            Icons.home,
+            'Home',
+            () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => HomePage()))
                 }),
-                CustomListTile(Icons.fastfood,'Food', ()=>{
-                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => FoodPage()))
+        CustomListTile(
+            Icons.fastfood,
+            'Food',
+            () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => FoodPage()))
                 }),
-                CustomListTile(Icons.event_note,'Calendar', ()=>{
-                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => CalendarPage()))
+        CustomListTile(
+            Icons.event_note,
+            'Calendar',
+            () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => CalendarPage()))
                 }),
-                //Icons.wb_sunny_outlined
-                CustomListTile(Icons.wb_sunny,'Weather', ()=>{
-                  Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => WeatherApp()))
+        //Icons.wb_sunny_outlined
+        CustomListTile(
+            Icons.wb_sunny,
+            'Weather',
+            () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => WeatherApp()))
                 }),
-                //Icons.logout
-                CustomListTile(Icons.exit_to_app,'Logout', 
-                  ()=>{{_auth.signOut(), Navigator.pushNamed(context, Login.id)}}),
-            ],)
-          );
+        CustomListTile(
+            Icons.book,
+            'Courses',
+            () => {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => CoursePage()))
+                }),
+        //Icons.logout
+        CustomListTile(
+            Icons.exit_to_app,
+            'Logout',
+            () => {
+                  {_auth.signOut(), Navigator.pushNamed(context, Login.id)}
+                }),
+      ],
+    ));
   }
 }
 
-class CustomListTile extends StatelessWidget{
-
+class CustomListTile extends StatelessWidget {
   IconData icon;
   String text;
   Function onTap;
 
-  CustomListTile(this.icon,this.text, this.onTap);
+  CustomListTile(this.icon, this.text, this.onTap);
 
   @override
   Widget build(BuildContext context) {
@@ -72,32 +114,32 @@ class CustomListTile extends StatelessWidget{
       padding: const EdgeInsets.all(8.0),
       child: Container(
         decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: Colors.grey.shade400))
-         ),
+            border: Border(bottom: BorderSide(color: Colors.grey.shade400))),
         child: InkWell(
           splashColor: shockerYellow,
-         onTap: onTap,
+          onTap: onTap,
           child: Container(
-           height: 50,
-           child: Row(
+            height: 50,
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-               Row(
+                Row(
                   children: [
                     Icon(icon),
-                   Padding(
+                    Padding(
                       padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-                      child: Text(text, style: TextStyle(
-                      fontSize: 16.0
-                    ),),
-                  ),
-                ],
-              ),
-              Icon(Icons.arrow_right)
-            ],
+                      child: Text(
+                        text,
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                ),
+                Icon(Icons.arrow_right)
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
