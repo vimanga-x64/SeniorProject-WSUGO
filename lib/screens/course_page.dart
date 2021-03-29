ffbff58e17aaa28f3b17f43361cf83a5e85081ee
HuyN1201 <huyn120198@gmail.com>
2021-03-28 22:27:55 -0500

    Initial code for Read course data

--- lib/screens/course_page.dart
+++ lib/screens/course_page.dart
@@ -3,9 +3,10 @@ import 'package:wsu_go/constants.dart';
 import 'package:flutter/material.dart';
 import './drawer.dart';
 import './add_course.dart';
-
-//import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
+// Import the firebase_auth plugin
+import 'package:firebase_auth/firebase_auth.dart';
+// Import Cloud Firestore package
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CoursePage extends StatefulWidget {
   static const String id = 'course_page';
@@ -14,6 +15,11 @@ class CoursePage extends StatefulWidget {
 }
 
 class _CoursePageState extends State<CoursePage> {
+  //Creating an instance for Firesore, students
+  //Tapping into our collection of Students
+  CollectionReference students =
+      FirebaseFirestore.instance.collection('Students');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +36,14 @@ class _CoursePageState extends State<CoursePage> {
       ),
       drawer: CustomDrawer(),
       floatingActionButton: AddButton(),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            StudentClasses(),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -48,6 +62,42 @@ class _AddButtonState extends State<AddButton> {
       },
       child: Icon(Icons.add),
       backgroundColor: shockerYellow,
+    );
+  }
+}
+
+class StudentClasses extends StatefulWidget {
+  @override
+  _StudentClassesState createState() => _StudentClassesState();
+}
+
+class _StudentClassesState extends State<StudentClasses> {
+  //Creating a Future function that is going to return a variable type of "QuerySnapshot"
+  //Future function is async with an awaiting variable of student that is type "QuerySnapshot"
+  Future<QuerySnapshot> getUserCourseData() async {
+    //Creating two final variables to access FirebaseAuthentication and Cloud Firestore
+
+    //Targets that current user that is logged in
+    final _auth = FirebaseAuth.instance.currentUser;
+
+    //Get the current logged in student's data
+    //They are targeted by their "_auth.uid"
+    final student = FirebaseFirestore.instance
+        .collection('Students')
+        .doc(_auth.uid)
+        .collection('Course')
+        .get();
+
+    return await student;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Returning FutureBuilder() widget that is going to assist on waiting for
+    return FutureBuilder(
+      //Set future variable as our Future<QuerySnapshot>
+      future: getUserCourseData(),
+      builder: (),
     );
   }
 }
